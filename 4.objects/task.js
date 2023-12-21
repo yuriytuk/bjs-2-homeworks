b36ee31a60e4093de8ad1f5c6af4d22f23fecf9b
Yuriy <yuriytukal@yandex.ru>
2023-12-21 05:01:32 +0300

    Задание исправлено

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -8,32 +8,25 @@ function Student(name, gender, age) {
   this.name = name;
   this.gender = gender;
   this.age = age;
-  this.subject =[];
   this.marks = [];
-  this.excluded;
 }
 
 Student.prototype.setSubject = function (subjectName) {
     this.subject = subjectName;
 }
   
-Student.prototype.addMarks = function (...marks) {
-  if (this.excluded == undefined) {
-    if (this.marks == undefined) {
-      this.marks = [...marks];
-    } else {
-      this.marks.push(...marks);
-    }
+Student.prototype.addMarks = function (...marksToAdd ) {
+  if (this.marks != undefined) {
+    this.marks.push(...marksToAdd);
   }
 }
 
 Student.prototype.getAverage = function () {
-    if (this.marks === undefined || this.marks.length === 0) {
-      return 0;
-    } else {
-      let result = this.marks.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
-      return result / this.marks.length;
-    }
+  if (this.marks != undefined && this.marks.length > 0) {
+    let result = this.marks.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+    return result / this.marks.length;
+  }
+  return 0;  
 }
 
 Student.prototype.exclude = function (reason) {
