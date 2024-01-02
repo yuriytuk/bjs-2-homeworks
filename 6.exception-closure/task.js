b937ec917aa1bc61faa66a3429d61d43d432837e
Yuriy <yuriytukal@yandex.ru>
2024-01-02 22:14:23 +0300

    Первое задание готово

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,47 @@
-﻿
+function parseCount(value) { 
+    let result = Number.parseFloat(value);
+    if (isNaN(result) === true) {
+        throw new Error("Невалидное значение");
+    }
+    return result;
+}
+
+function validateCount(value) {
+    try {
+        return parseCount(value);
+    } catch (error) {
+        return error;
+    }
+}
+
+class Triangle {
+    constructor(a, b, c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        if ((this.a + this.b) < this.c || (this.b + this.c) < this.a || (this.a + this.c) < this.b) {
+            throw new Error ("Треугольник с такими сторонами не существует");
+        }
+    }
+
+    get perimeter () {
+        return this.a + this.b + this.c;
+    }
+
+    get area() {
+        let p = (this.a + this.b + this.c) * 0.5;
+        let d = Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
+        const k = d.toFixed(3);
+        return k;
+    }
+}
+
+let a1 = new Triangle(a, b, c);
+function getTriangle (a, b, c) {
+    
+    try {
+        return a1.perimeter;
+    } catch (error) {
+        return error;
+    }
+}
